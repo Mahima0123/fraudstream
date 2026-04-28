8d67c9fad2b4e37fd5354c2a7de662e4cc157b7b
Mahima Chaudhary <chaudharymahima983@gmail.com>
2026-04-27 21:58:23 -0500

    rule based anomaly detection

--- consumer/transaction_consumer.py
+++ consumer/transaction_consumer.py
@@ -1,5 +1,7 @@
 from kafka import KafkaConsumer
 import json
+from collections import defaultdict, deque
+from datetime import datetime, timedelta
 
 consumer = KafkaConsumer(
     "transactions",
@@ -9,7 +11,35 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-print("Consuming transactions...")
+print("Consumer initialized...")
+
+# store recent transactions per user
+user_transactions = defaultdict(list)
+
+# thresholds
+HIGH_AMOUNT = 1500
+TIME_WINDOW = timedelta(minutes=1)
+MAX_TXN_COUNT = 5
 
 for message in consumer:
-    print(message.value)
+    event = message.value
+    user = event['user_id']
+    amount = event['amount']
+    timestamp = datetime.fromisoformat(event['timestamp'])
+
+    # store transaction
+    user_transactions[user].append((timestamp, amount))
+
+    # remove old transactions
+    user_transactions[user] = [
+        txn for txn in user_transactions[user]
+        if timestamp - txn[0] <= TIME_WINDOW
+    ]
+
+    # 1. Hight value transaction
+    if amount > HIGH_AMOUNT:
+        print(f"ALERT: High value transaction detected for {event}")
+    
+    # 2. Too many transactions in short time
+    if len(user_transactions[user]) > MAX_TXN_COUNT:
+        print(f"ALERT: Too many transactions detected for user {event}")
