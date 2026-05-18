e833833ccd29a938228573ba7b4ad2f97a86e34a
Mahima Chaudhary <chaudharymahima983@gmail.com>
2026-05-18 00:05:04 -0500

    database correction

--- utils/db.py
+++ utils/db.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
+        host="127.0.0.1",
         database="fraudstream",
         user="fraud_user",
         password="fraud_pass",
-        port=5432
+        port=5433
     )
